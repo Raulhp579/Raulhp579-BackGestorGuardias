f45e069dc4385f6ceb0fc88a57b2572aa7c4918c
Raulhp <raulhenares5@gamil.com>
2026-03-25 19:32:13 +0100

    prueba cambios web

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,13 @@ use App\Http\Controllers\PdfController;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/app', fn () => redirect('/app/'));
+/* Route::get('/app', fn () => redirect('/app/')); mirar si da error en produccion*/
 
 Route::get('/app/{any?}', function () {
     return File::get(public_path('app/index.html'));
 })->where('any', '.*');
 
-Route::get('/', fn () => redirect('/app/'));
+Route::get('/', fn () => redirect('/app'));
 
 // Ruta para ver la plantilla del PDF de guardias
 Route::get('/plantilla-dia-pdf', [PdfController::class, 'generarPdfDia']);
@@ -22,6 +22,7 @@ Route::get('/sendEmail', [MailController::class, 'send']);
 Route::get('/{any}', function () {
     return File::get(public_path('app/index.html'));
 })->where('any', '^(?!api).*$');
+
 
 /* Route::get('/importUsers', [ImportExcelsController::class, "importWorkers"])->name('import.users');
 Route::post('/importDutys',[ImportExcelsController::class, "importDutys"])->name('import.dutys'); */
